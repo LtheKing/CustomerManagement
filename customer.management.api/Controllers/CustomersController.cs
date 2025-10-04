c90e969fd1edc5361ec9e772faf17ba0b7c210c5
tdananto <tdananto@pmintl.net>
2025-10-04 17:08:13 +0700

    add Customer traffic endpoints

--- customer.management.api/Controllers/CustomersController.cs
+++ customer.management.api/Controllers/CustomersController.cs
@@ -27,6 +27,31 @@ namespace customer.management.api.Controllers
                 .ToListAsync();
         }
 
+        // GET: api/customers/traffic
+        [HttpGet("traffic")]
+        public async Task<ActionResult<IEnumerable<CustomerTrafficModelEntity>>> GetCustomerTraffic()
+        {
+            return await _context.CustomerTraffic
+                .Include(ct => ct.Customer)
+                .ToListAsync();
+        }
+
+        // GET: api/customers/traffic/5
+        [HttpGet("traffic/{id}")]
+        public async Task<ActionResult<CustomerTrafficModelEntity>> GetCustomerTraffic(Guid id)
+        {
+            var customerTraffic = await _context.CustomerTraffic
+                .Include(ct => ct.Customer)
+                .FirstOrDefaultAsync(ct => ct.Id == id);
+
+            if (customerTraffic == null)
+            {
+                return NotFound();
+            }
+
+            return customerTraffic;
+        }
+
         // GET: api/customers/5
         [HttpGet("{id}")]
         public async Task<ActionResult<CustomerModelEntity>> GetCustomer(Guid id)
@@ -101,9 +126,70 @@ namespace customer.management.api.Controllers
             return NoContent();
         }
 
+        // POST: api/customers/traffic
+        [HttpPost("traffic")]
+        public async Task<ActionResult<CustomerTrafficModelEntity>> PostCustomerTraffic(CustomerTrafficModelEntity customerTraffic)
+        {
+            _context.CustomerTraffic.Add(customerTraffic);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomerTraffic", new { id = customerTraffic.Id }, customerTraffic);
+        }
+
+        // PUT: api/customers/traffic/5
+        [HttpPut("traffic/{id}")]
+        public async Task<IActionResult> PutCustomerTraffic(Guid id, CustomerTrafficModelEntity customerTraffic)
+        {
+            if (id != customerTraffic.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(customerTraffic).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerTrafficExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/customers/traffic/5
+        [HttpDelete("traffic/{id}")]
+        public async Task<IActionResult> DeleteCustomerTraffic(Guid id)
+        {
+            var customerTraffic = await _context.CustomerTraffic.FindAsync(id);
+            if (customerTraffic == null)
+            {
+                return NotFound();
+            }
+
+            _context.CustomerTraffic.Remove(customerTraffic);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
         private bool CustomerExists(Guid id)
         {
             return _context.Customers.Any(e => e.Id == id);
+        }
+
+        private bool CustomerTrafficExists(Guid id)
+        {
+            return _context.CustomerTraffic.Any(e => e.Id == id);
         }
     }
 }
