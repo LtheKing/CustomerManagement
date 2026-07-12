e59f8e6dd06fea448c1930d47757d6999cc1c990
aldinata7 <leonalding77@gmail.com>
2026-07-12 09:16:05 +0700

    make app downloadable

--- customer.management.front.end/src/main.tsx
+++ customer.management.front.end/src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch((error) => {
+      console.warn("Service worker registration failed:", error);
+    });
+  });
+}
+
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
